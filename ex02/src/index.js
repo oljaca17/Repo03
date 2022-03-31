4c9c382b00020eeec27b6cf4a12932c50cb65300
unknown <sasaoljaca85@gmail.com>
2022-03-31 09:42:33 +0200

    added ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    //Only change code below this line
+    //Only change code below this line
+  
+    // this is my first comment
+    /* this is my second comment*/
+}
+
+main();
+module.exports = main;
+
