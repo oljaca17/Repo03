6400769b48d05ca7e7f825d5a2d22bc3f8d67100
unknown <sasaoljaca85@gmail.com>
2022-03-31 10:09:12 +0200

    update

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,14 +1,11 @@
 function main() {
     //Only change code below this line
-
-    // this is my first comment
-    /* this is my second comment*/
-
+var forCat;
     //Only change code above this line
   
-  
+
+
 }
 
 console.log(main());
 module.exports = main;
-
