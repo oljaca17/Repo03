5403f6441a18a8eb7fef222b25ce738b85718dbc
unknown <sasaoljaca85@gmail.com>
2022-03-31 11:02:57 +0200

    update

---
+++ ex08/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+    var numOne;
+    var numTwo;
+    var sum;
+
+    numOne = 8;
+    numTwo = 15;
+    sum = numOne + numTwo;
+    //Only change code above this line
+
+return sum;
+
+}
+
+console.log(main(numOne, numTwo));
+module.exports = main;
